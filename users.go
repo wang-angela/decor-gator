cc1219431b7a261694939c059de665e6e301938a
Angela <92885742+wang-angela@users.noreply.github.com>
2023-02-05 18:13:15 -0500

    Implement password encryption on PUT method

--- users.go
+++ users.go
@@ -58,9 +58,19 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln("User not found")
 	}
 
+	// Retrieve and store current
+	cur := user.Password
+
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		log.Fatalln("Error Decoding")
+	}
+
+	// If password didn't change, keep current hash
+	if comparePassword(user.Password, cur) {
+		user.Password = cur
+	} else {
+		user.Password = encrypt(user.Password)
 	}
 
 	db.Save(&user)
