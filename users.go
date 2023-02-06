a9d994741a9bfc3d978101b85c83ee4666f2715e
Angela <92885742+wang-angela@users.noreply.github.com>
2023-02-05 20:36:22 -0500

    Add comments and explain code

--- users.go
+++ users.go
@@ -18,9 +18,11 @@ type User struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+	Email    string `json:"email"`
 }
 
 func initMigration() {
+	// Open data.db; if data does not exist, create it
 	db, err = gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
 	// If there is an error, print error and message
 	if err != nil {
@@ -34,6 +36,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
+	// Returns error if decoding is unsuccessful
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		log.Fatalln("Error Decoding")
@@ -41,6 +44,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 
 	db.Create(&user)
 
+	// Returns error if encoding is unsuccessful
 	err = json.NewEncoder(w).Encode(&user)
 	if err != nil {
 		log.Fatalln("Error Encoding")
@@ -51,6 +55,7 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	w.Header().Set("Content-Type", "application/json")
 
+	// Search for user by id; id=0 if user not found
 	params := mux.Vars(r)["id"]
 	db.First(&user, params)
 	if user.ID == 0 {
