84325def705b60d7d6a89eb037c18c7c77801eb5
Sam Gallic <samgallic@ufl.edu>
2023-02-07 18:40:43 -0500

    Created the get methods and delete method.

--- main.go
+++ main.go
@@ -15,8 +15,11 @@ func main() {
 func initRouter() {
 	r := mux.NewRouter()
 
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", getUser).Methods("GET")
 	r.HandleFunc("/users", createUser).Methods("POST")
 	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
