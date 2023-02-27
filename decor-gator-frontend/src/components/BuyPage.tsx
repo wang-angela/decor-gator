c9e39bf186350caac94eb5a402ae092e8db53df8
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-02-27 15:17:41 -0500

    update progress on buy page

--- decor-gator-frontend/src/components/BuyPage.tsx
+++ decor-gator-frontend/src/components/BuyPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import PostPage from "./PostPage"
+import Header from "./Header"
 
 //We might need class instead of function; In that case, find out how to use navigate in class component
 function BuyPage() {
@@ -8,9 +8,12 @@ function BuyPage() {
     //Build more structure
     const navigate = useNavigate();
     return (
-        <button type="button" className="makePost-button" onClick={()=>navigate('/PostPage')}>
-            + Post
-        </button>
+        <div>
+            <Header />cd 
+            <button type="button" className="makePost-button" onClick={()=>navigate('/PostPage')}>
+                + Post
+            </button>
+        </div>
     )
 }
 
---
+++ decor-gator-frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        // when clicked, send to main page
+        //change text to logo image
+        <div className="header">
+            Decor Gator
+        </div>
+
+    )
+}
+
+export default Header
