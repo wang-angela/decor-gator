4de3e12c80c6b43f48b338af2758d0a15381535c
Angela <92885742+wang-angela@users.noreply.github.com>
2023-02-27 16:19:39 -0500

    Implement first unit test for POST

---
+++ backend/users_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func initDB() {
+	// Open data.db; if data does not exist, create it
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	// If there is an error, print error and message
+	if err != nil {
+		log.Print("Unable to connect to DB")
+	}
+	db.AutoMigrate(&User{}, &Post{})
+}
+
+func clearDB() {
+	db.Exec("DELETE FROM users")
+	db.Exec("DELETE FROM posts")
+}
+
+func TestCreateUser(t *testing.T) {
+	initDB()
+	clearDB()
+
+	// Request Body
+	jsonBody := []byte(`{
+		"username": "john.smith",
+		"password": "123abc",
+		"email":    "john.smith@gmail.com"
+	}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	// Send new request with json body info
+	req, err := http.NewRequest("POST", "/users", bodyReader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Record Response
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(createUser)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Decoding recorded response
+	var resp map[string]interface{}
+	json.Unmarshal(rr.Body.Bytes(), &resp)
+
+	if resp["username"] != "john.smith" {
+		t.Errorf("username is invalid, expected john.smith, got %v", resp["username"])
+	}
+}
