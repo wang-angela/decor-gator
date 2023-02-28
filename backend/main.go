d31f841aba6bbcbfc4ef897cd0ca10ac1ba1e9b2
crugenstein <crugenstein@ufl.edu>
2023-02-27 21:32:59 -0500

    signup handling

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	// "github.com/rs/cors"
+	"github.com/rs/cors"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -55,13 +55,13 @@ func initRouter() {
 	r.HandleFunc("/images/{id}", updateImage).Methods("PUT")
 	r.HandleFunc("/images/{id}", deleteImage).Methods("DELETE")
 
-	/*
+	
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowCredentials: true,
 	})
-	*/
+	
 
 	handler := c.Handler(r)
 	log.Fatal(http.ListenAndServe(":8080", handler))
