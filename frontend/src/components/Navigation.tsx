c02e9f26108853e37e611d04d081782c81527da3
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-02-28 00:03:12 -0500

    folder name change

--- frontend/src/components/Navigation.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-//Use this as collection of methods with redirecting functions
-//Or a place to check if redirecting is allowed (whether or not user is authenticated)
-export default function Navigation() {
-
-}
