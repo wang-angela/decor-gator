21236d5485ac5dc23ed8fb28ae4a6e83e535600f
crugenstein <crugenstein@ufl.edu>
2023-02-28 23:02:18 -0500

    post handling (to and from backend)

---
+++ decor-gator-frontend/src/PostPage.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function PostPage() {
+
+    const postTitleRef = React.createRef<HTMLInputElement>()
+    const postTypeRef = React.createRef<HTMLInputElement>()
+
+    function uploadPost() {
+        var Title, FurnitureType, UserPosted, postObj
+        if (postTitleRef.current && postTypeRef.current) {
+            Title = postTitleRef.current.value
+            FurnitureType = postTypeRef.current.value
+            console.log(JSON.parse(localStorage.getItem('userData') || ""))
+            let a = JSON.parse(localStorage.getItem('userData') || "")
+            UserPosted = JSON.parse(localStorage.getItem('userData') || "").email
+            postObj = {Title, FurnitureType, UserPosted}
+        }
+        if (!Title || !FurnitureType) {
+            alert("Please enter all fields.")
+        } else {
+            fetch('http://localhost:8080/posts', {
+            method: "POST",
+            headers: {'content-type': 'application/json'},
+            body:JSON.stringify(postObj)
+        }).then((response)=>{
+            console.log(response)
+            alert("Post successfully created!")
+        }).catch((err) => {
+            console.log(err)
+        })
+        }
+
+    }
+
+    return (
+        <div className = 'post-editor'>
+            <input ref={postTitleRef} type='text' placeholder='Title' className='post-title' />
+            <input ref={postTypeRef} type='text' placeholder='Furniture Type' className='post-furniture-type' />
+            <button type='button' onClick={uploadPost} className='post-submit-button'>Submit Post</button>
+        </div>
+    )
+}
