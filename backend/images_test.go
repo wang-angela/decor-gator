b763aa3ee1521c3bc26081ddc29d7b3818fb90e7
Angela <92885742+wang-angela@users.noreply.github.com>
2023-03-01 16:37:59 -0500

    Add unit-test for Image class

--- backend/images_test.go
+++ backend/images_test.go
@@ -10,18 +10,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func TestGetAllPosts(t *testing.T) {
+func TestGetAllImages(t *testing.T) {
 	initDB()
 
 	// Send new request with json body info
-	req, err := http.NewRequest("POST", "/users", nil)
+	req, err := http.NewRequest("GET", "/images", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Record Response
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(getPosts)
+	handler := http.HandlerFunc(getImages)
 
 	handler.ServeHTTP(rr, req)
 
@@ -31,21 +31,21 @@ func TestGetAllPosts(t *testing.T) {
 	}
 
 	// Decoding recorded response
-	var resp []Post
+	var resp []Image
 	if err := json.Unmarshal(rr.Body.Bytes(), &resp); err != nil {
-		t.Errorf("Invalid response, expected list of posts, got %v", rr.Body.String())
+		t.Errorf("Invalid response, expected list of images, got %v", rr.Body.String())
 	}
 
 	if len(resp) < 1 {
-		t.Errorf("Invalid number of posts, expected 1, got %v", len(resp))
+		t.Errorf("Invalid number of images, expected 1, got %v", len(resp))
 	}
 }
 
-func TestGetPost(t *testing.T) {
+func TestGetImage(t *testing.T) {
 	initDB()
 
 	// Send new request with json body info
-	req, err := http.NewRequest("GET", "/posts/1", nil)
+	req, err := http.NewRequest("GET", "/images/1", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -54,7 +54,7 @@ func TestGetPost(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/posts/{id}", getPost)
+	r.HandleFunc("/images/{id}", getImage)
 	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
@@ -66,33 +66,32 @@ func TestGetPost(t *testing.T) {
 	var resp map[string]interface{}
 	json.Unmarshal(rr.Body.Bytes(), &resp)
 
-	if resp["title"] != "Sofa for sale!" {
-		t.Errorf("Posts is invalid, expected \"Sofa for sale\", got %v", resp["title"])
+	const mockData = "joewjoiu432987(*&(*$#&*(Wuroiesjfkjaskjdoiwj"
+	if resp["ByteData"] != mockData {
+		t.Errorf("Data is invalid, expected \"joewjoiu432987(*&(*$#&*(Wuroiesjfkjaskjdoiwj\" got %v", resp["image_byte_data"])
 	}
 }
 
-func TestCreatePost(t *testing.T) {
+func TestCreateImage(t *testing.T) {
 	initDB()
 	tx.SavePoint("sp1")
 
 	// Request Body
 	jsonBody := []byte(`{
 		"id": 4,
-		"title": "Hello Kitty Chair for Rent!",
-		"furnitureType": "Chair",
-		"userPosted": "john.smith"
+		"ByteData": "hfji3u42987&(*&$#@(*Q&$ilkasmlkamljdaljo"
 	}`)
 	bodyReader := bytes.NewReader(jsonBody)
 
-	// Send new request with json body info
-	req, err := http.NewRequest("POST", "/posts", bodyReader)
+	// Send new request whfji3u42987&(*&$#@(*Q&$ilkasmlkamljdaljoith json body info
+	req, err := http.NewRequest("POST", "/images", bodyReader)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Record Response
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(createPost)
+	handler := http.HandlerFunc(createImage)
 
 	handler.ServeHTTP(rr, req)
 
@@ -105,28 +104,27 @@ func TestCreatePost(t *testing.T) {
 	var resp map[string]interface{}
 	json.Unmarshal(rr.Body.Bytes(), &resp)
 
-	if resp["title"] != "Hello Kitty Chair for Rent!" {
-		t.Errorf("Post title is invalid, expected \"Hello Kitty Chair for Rent!\", got %v", resp["title"])
+	const mockData = "hfji3u42987&(*&$#@(*Q&$ilkasmlkamljdaljo"
+	if resp["ByteData"] != mockData {
+		t.Errorf("Image title is invalid, expected \"hfji3u42987&(*&$#@(*Q&$ilkasmlkamljdaljo\", got %v", resp["ByteData"])
 	}
 
 	tx.RollbackTo("sp1")
 }
 
-func TestUpdatePost(t *testing.T) {
+func TestUpdateImage(t *testing.T) {
 	initDB()
 	tx.SavePoint("sp2")
 
 	// Request Body
 	jsonBody := []byte(`{
 		"id": 2,
-		"title": "Selling a Dining Table",
-		"furnitureType": "Table",
-		"userPosted": "william-scott"
+		"ByteData": "dskjfo34($&*378uwefoisdjfoiwqurifj"
 	}`)
 	bodyReader := bytes.NewReader(jsonBody)
 
 	// Send new request with json body info
-	req, err := http.NewRequest("PUT", "/posts/2", bodyReader)
+	req, err := http.NewRequest("PUT", "/images/2", bodyReader)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -135,7 +133,7 @@ func TestUpdatePost(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/posts/{id}", updatePost)
+	r.HandleFunc("/images/{id}", updateImage)
 	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
@@ -147,19 +145,20 @@ func TestUpdatePost(t *testing.T) {
 	var resp map[string]interface{}
 	json.Unmarshal(rr.Body.Bytes(), &resp)
 
-	if resp["title"] != "Selling a Dining Table" {
-		t.Errorf("Post Title is invalid, expected \"Selling a Dining Table\", got %v", resp["title"])
+	const mockData = "dskjfo34($&*378uwefoisdjfoiwqurifj"
+	if resp["ByteData"] != mockData {
+		t.Errorf("Image Title is invalid, expected \"dskjfo34($&*378uwefoisdjfoiwqurifj\", got %v", resp["title"])
 	}
 
 	tx.RollbackTo("sp2")
 }
 
-func TestDeletePost(t *testing.T) {
+func TestDeleteImage(t *testing.T) {
 	initDB()
 	tx.SavePoint("sp3")
 
 	// Send new request with json body info
-	req, err := http.NewRequest("DELETE", "/posts/3", nil)
+	req, err := http.NewRequest("DELETE", "/images/3", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -168,7 +167,7 @@ func TestDeletePost(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/posts/{id}", deletePost)
+	r.HandleFunc("/images/{id}", deleteImage)
 	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
