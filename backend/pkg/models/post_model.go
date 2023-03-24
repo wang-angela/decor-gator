4067077d51888fa220b5db3ba30f2fa55698f4c0
Sam Gallic <samgallic@ufl.edu>
2023-03-24 15:36:22 -0400

    Minor changes to the post struct

--- backend/pkg/models/post_model.go
+++ backend/pkg/models/post_model.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
-	ID            uint   `gorm:"primaryKey" json:"id"`
-	Title         string `json:"title"`
-	FurnitureType string `json:"furnitureType"`
-	UserPosted    string `json:"userPosted"` // Change later to user object once we learn how to test that on Postman.
+	ID            uint    `gorm:"primaryKey" json:"id"`
+	Title         string  `json:"title"`
+	FurnitureType string  `json:"furnitureType"`
+	Description   string  `json:"description"`
+	Price         float64 `json:"price"`
+	UserPosted    string  `json:"userPosted"` // Change later to user object once we learn how to test that on Postman.
 }
