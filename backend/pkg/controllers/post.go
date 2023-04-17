e4fc694680e04bb2fc82ec9a9b666551a7b27965
Sam Gallic <samgallic@ufl.edu>
2023-04-17 15:45:12 -0400

    Modified to make post class compatible with new database

--- backend/pkg/controllers/post.go
+++ backend/pkg/controllers/post.go
@@ -1,27 +1,39 @@
 package controllers
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/decor-gator/backend/pkg/configs"
 	"github.com/decor-gator/backend/pkg/models"
-	"github.com/decor-gator/backend/pkg/utils"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var postColl *mongo.Collection = configs.GetCollection(configs.DB, "posts")
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application-json")
 	var posts []models.Post
 
-	// Prints an error if no posts are in the database.
-	if utils.DB.Find(&posts).Error != nil {
-		log.Printf("No posts exists.")
+	// Retrieving posts
+	cur, err := postColl.Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	err = json.NewEncoder(w).Encode(posts)
+	// Decodes results into Post array
+	if cur.All(context.TODO(), &posts) != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewEncoder(w).Encode(&posts)
 	if err != nil {
-		log.Fatalln("Error Encoding")
+		log.Printf("Error Encoding.")
 	}
 }
 
@@ -29,11 +41,23 @@ func GetPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application-json")
 	var post models.Post
 
-	// Prints error if the post doesn't exist.
+	// Prints an error id the post doesn't exists.
 	params := mux.Vars(r)["id"]
-	utils.DB.First(&post, params)
-	if post.ID == 0 {
-		log.Fatalln("Post not found")
+	filter := bson.D{{Key: "id", Value: params}}
+
+	err := postColl.FindOne(context.TODO(), filter).Decode(&post)
+	if err == mongo.ErrNoDocuments {
+		// Throws error if no post exists
+		msg := "Post does not exist"
+
+		err = json.NewEncoder(w).Encode(&msg)
+		if err != nil {
+			log.Fatalln("Error Encoding")
+		}
+		return
+	} else if err != nil {
+		// Throws error for other cases
+		log.Fatal(err)
 	}
 
 	err = json.NewEncoder(w).Encode(&post)
@@ -53,12 +77,17 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln("Error Decoding")
 	}
 
-	if utils.DB.Create(&post).Error != nil {
-		log.Println("Post already exists")
+	// Generating id
+	post.ID = primitive.NewObjectID()
+
+	// Insert into MongoDB
+	res, err := postColl.InsertOne(context.TODO(), post)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Returns error if encoding is unsuccessful
-	err = json.NewEncoder(w).Encode(&post)
+	err = json.NewEncoder(w).Encode(res)
 	if err != nil {
 		log.Fatalln("Error Encoding")
 	}
@@ -68,21 +97,41 @@ func UpdatePost(w http.ResponseWriter, r *http.Request) {
 	var post models.Post
 	w.Header().Set("Content-Type", "application/json")
 
-	// Search for user by id; id=0 if user not found
-	params := mux.Vars(r)["id"]
-	utils.DB.First(&post, params)
-	if post.ID == 0 {
-		log.Fatalln("Post not found")
-	}
-
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
 		log.Fatalln("Error Decoding")
 	}
 
-	utils.DB.Save(&post)
+	// Prints an error id the post doesn't exists.
+	params := mux.Vars(r)["_id"]
+	filter := bson.D{{Key: "_id", Value: params}}
 
-	err = json.NewEncoder(w).Encode(&post)
+	update := bson.D{{Key: "$set",
+		Value: bson.D{
+			{Key: "_id", Value: post.ID},
+			{Key: "title", Value: post.Title},
+			{Key: "furnitureType", Value: post.FurnitureType},
+			{Key: "description", Value: post.Description},
+			{Key: "price", Value: post.Price},
+			{Key: "userPosted", Value: post.UserPosted},
+			{Key: "imageURL", Value: post.ImageURL},
+		},
+	}}
+
+	res, err := postColl.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	} else if res.MatchedCount == 0 {
+		msg := "Post does not exist"
+
+		err = json.NewEncoder(w).Encode(&msg)
+		if err != nil {
+			log.Fatalln("Error Encoding")
+		}
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(res)
 	if err != nil {
 		log.Fatalln("Error Encoding")
 	}
@@ -90,13 +139,27 @@ func UpdatePost(w http.ResponseWriter, r *http.Request) {
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application-json")
-	var post models.Post
-	params := mux.Vars(r)["id"]
 
-	// Prints if deletion was not successful.
-	if utils.DB.Delete(&post, params).Error != nil {
-		log.Printf("Could not delete post.")
+	// Search parameters
+	params := mux.Vars(r)["_id"]
+	filter := bson.D{{Key: "_id", Value: params}}
+
+	res, err := postColl.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		// Throws error for other cases
+		log.Fatal(err)
+	} else if res.DeletedCount == 0 {
+		msg := "Failed to delete"
+
+		err = json.NewEncoder(w).Encode(&msg)
+		if err != nil {
+			log.Fatalln("Error Encoding")
+		}
+		return
 	}
 
-	json.NewEncoder(w).Encode("You've successfully deleted this post.")
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		log.Fatalln("Error Encoding")
+	}
 }
