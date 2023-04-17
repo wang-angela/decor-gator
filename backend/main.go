21cb1b0683ac7a609ce2f326a150b0496230d8b5
Angela <92885742+wang-angela@users.noreply.github.com>
2023-04-17 18:23:39 -0400

    Remove unneccesary files

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/decor-gator/backend/pkg/configs"
-	"github.com/decor-gator/backend/pkg/controllers"
 	"github.com/decor-gator/backend/pkg/routes"
 	"github.com/decor-gator/backend/pkg/utils"
 	"github.com/gorilla/mux"
@@ -16,33 +13,6 @@ import (
 )
 
 func main() {
-	folder := "test-pictures"
-
-	controllers.Init()
-	controllers.CreateBucket()
-
-	for _, bucket := range controllers.ListBuckets().Buckets {
-		fmt.Println(*bucket.Name)
-	}
-
-	files, _ := ioutil.ReadDir(folder)
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		} else {
-			controllers.UploadObject(folder + "/" + file.Name())
-		}
-	}
-
-	fmt.Println(controllers.ListObjects())
-
-	for _, object := range controllers.ListObjects().Contents {
-		controllers.GetObject(*object.Key)
-		controllers.DeleteObject(*object.Key)
-	}
-
-	fmt.Println(controllers.ListObjects())
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Print("Error loading .env")
