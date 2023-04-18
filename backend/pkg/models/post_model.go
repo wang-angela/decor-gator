d322127f5276140fb9570fbfdc634d6a980f302e
Angela <92885742+wang-angela@users.noreply.github.com>
2023-04-18 15:28:18 -0400

    Change json variables

--- backend/pkg/models/post_model.go
+++ backend/pkg/models/post_model.go
@@ -5,9 +5,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Post struct {
 	ID            primitive.ObjectID `json:"_id" bson:"_id"`
 	Title         string             `json:"title" bson:"title"`
-	FurnitureType string             `json:"furniture_type" bson:"furniture_type"`
+	FurnitureType string             `json:"furnitureType" bson:"furniture_type"`
 	Description   string             `json:"description" bson:"description"`
 	Price         float64            `json:"price" bson:"price"`
-	UserPosted    string             `json:"user_posted" bson:"user_posted"` // Change later to user object once we learn how to test that on Postman.
-	ImageURL      string             `json:"image_url" bson:"image_url"`
+	UserPosted    string             `json:"userPosted" bson:"user_posted"` // Change later to user object once we learn how to test that on Postman.
+	ImageURL      string             `json:"imageURL" bson:"image_url"`
 }
--- backend/pkg/models/user_model.go
+++ backend/pkg/models/user_model.go
@@ -4,8 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	FirstName string             `json:"first_name" bson:"first_name"`
-	LastName  string             `json:"last_name" bson:"last_name"`
+	FirstName string             `json:"firstName" bson:"first_name"`
+	LastName  string             `json:"lastName" bson:"last_name"`
 	Email     string             `json:"email" bson:"email"`
 	Username  string             `json:"username" bson:"username"`
 	Password  string             `json:"password" bson:"password"`
